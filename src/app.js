311bf9e0aed81790af0f41da96d67169386a2907
lso5507 <49707719+lso5507@users.noreply.github.com>
2025-01-26 20:23:46 +0900

    feat:HttpChunkTest

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 const express = require('express');
-
+const axios = require("axios")
 const app = express();
 
 app.set('port', process.env.PORT || 3001);
 
 app.get('/', (req, res) => {
     res.send('Hello, Express')
+});
+// Http/Chunk 방식 테스트
+app.get('/chat', async (req, res) => {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    //Setting HTTP/Chunk
+    res.setHeader('Transfer-Encoding', 'chunked');
+    try{
+        const response = await axios({
+            method: 'post',
+            url: 'http://localhost:5001/chat',
+            data: { message: "Hello AI!" },
+            responseType: 'stream',
+        });
+        
+        response.data.on('data', (chunk) => {
+            console.debug("chunk::",chunk.toString())
+            res.write(chunk.toString());
+        });
+    
+        response.data.on('end', () => {
+            console.debug("end");
+            res.end();
+        });
+    }catch (error){
+        console.error("/Chat Error",error);
+        res.status(500).send('Failed to Request.');
+    }
+  
 });
 
 app.listen(app.get('port'), ()=>{
